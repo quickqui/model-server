f963f0205e247091c686d8e9a8d3518a44bfeade
nielinjie <nielinjie@gmail.com>
2019-05-10 09:05:30 +0800

    model。

--- src/DataSchema.ts
+++ src/DataSchema.ts
@@ -1,7 +1,7 @@
 
 import { parse, DocumentNode, ObjectTypeDefinitionNode, FieldDefinitionNode, TypeNode, DirectiveNode, ListTypeNode, NonNullTypeNode, NamedTypeNode } from 'graphql'
 
-export function parseFromSchema(source:string): DataModel {
+export function parseFromSchema(source: string): DataModel {
     const document = parse(source)
     return new DataModel(document)
 }
@@ -10,14 +10,14 @@ export function parseFromSchema(source:string): DataModel {
 export class DataModel {
     types!: TypeDef[]
     constructor(document: DocumentNode) {
-        this.types = document.definitions.filter((def) => 
-             def.kind == "ObjectTypeDefinition"
+        this.types = document.definitions.filter((def) =>
+            def.kind == "ObjectTypeDefinition"
         ).map((it) =>
             new TypeDef(it as ObjectTypeDefinitionNode)
         )
     }
-    public type(name:string): TypeDef | undefined {
-        return this.types.find((it)=>   it.name == name)
+    public type(name: string): TypeDef | undefined {
+        return this.types.find((it) => it.name == name)
     }
 }
 class TypeDef {
@@ -40,28 +40,32 @@ class Field {
     name!: string;
     typeName!: TypeName;
     directives!: Directive[];
-    flags!:FieldFlag[]
+    flags!: FieldFlag[]
     constructor(node: FieldDefinitionNode) {
         this.name = node.name.value
         this.typeName = new TypeName(node.type)
         this.directives = node.directives ? node.directives.map((it) => new Directive(it)) : []
         this.flags = []
-        if(this.directives.find( d => d.name == "id")){
+        if (this.directives.find(d => d.name == "id")) {
             this.flags.push("id")
         }
-        if(this.directives.find(d=> d.name == "relation")){
+        if (this.directives.find(d => d.name == "relation")) {
             this.flags.push("relation")
+        }
+        if (this.directives.find(d => d.name === "brief")) {
+            this.flags.push("brief")
         }
     }
 }
 
-type FieldFlag = "id" | "name" | "relation"
-
+type FieldFlag = "id" | "brief" | "relation"
+const scalarTypes = ["String", "", "Int", "Float", "Boolean", "DateTime", "ID"]
 
 class TypeName {
     name!: string;
     isList!: boolean;
     isNotNull!: boolean;
+    isScalar!: boolean
 
     constructor(node: TypeNode) {
         //TODO 需要加入一些其他情况，比如list的list
@@ -70,13 +74,14 @@ class TypeName {
         this.name = this.getName(node)
         this.isList = this.getIsList(node)
         this.isNotNull = node.kind == "NonNullType"
+        this.isScalar = scalarTypes.includes(this.name)
     }
-    getIsList(node:TypeNode):boolean {
+    getIsList(node: TypeNode): boolean {
         if (node.kind == "ListType") return true
         if (node.kind == "NonNullType") return this.getIsList((node as NonNullTypeNode).type)
-        return false 
+        return false
     }
-    
+
 
     getName(node: TypeNode): string {
         if (node.kind == "ListType") return this.getName((node as ListTypeNode).type)
