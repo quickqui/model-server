37439637cd135fe4eef76d91414e976a989695f1
nielinjie <nielinjie@gmail.com>
2019-08-13 21:44:44 +0800

    presentation模型初步

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -1,10 +1,11 @@
 import { FunctionModel } from "../function/FunctionModel";
 import { DomainModel } from "../domain/DomainModel";
+import { PresentationModel } from "../presentation/PresentationModel";
 
 export interface Model {
     domainModel: DomainModel | undefined
     functionModel: FunctionModel | undefined
-
+    presentationModel: PresentationModel | undefined
     
 }
 
