6338f6e064bb199e978773a0e18de7897b444576
nielinjie <nielinjie@gmail.com>
2020-01-27 12:25:45 +0800

    小review

--- src/dynamic/Define.ts
+++ src/dynamic/Define.ts
@@ -14,7 +14,7 @@ export const REF_RESOLVE = "resolve";
 export const REF_REST = "rest";
 export const REF_PROVIDED = "provided";
 export type Ref = string;
-//TODO model-define 也有一个，需要统一起来。
+//TODO model-define 也有一个，是否需要统一起来？
 export interface RefObject {
   protocol: string | undefined;
   path: string;
